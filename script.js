a77773a90206c667973a257ec72072180eba857d
abbabashir <abbabashir516@gmail.com>
2023-10-26 15:51:15 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const path = require("path")
+const mysql = require("mysql")
+const dotenv = require("dotenv")
+
+dotenv.config({ path: './.env'})
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory))
+
+app.set('view engine', 'hbs')
+
+db.connect((error)=>{
+    if (error) {
+        console.log(error);
+    } else{
+        console.log("MYSQL connected...!!");
+    }
+})
+
+// DEFINE ROUTES
+app.use('/', require('./routes/pages'));
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+})
